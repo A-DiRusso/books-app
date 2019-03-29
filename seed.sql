dfe384162518b06535f09e3fee59f26f43ca41a3
Anthony DiRusso <ossurid@gmail.com>
2019-03-29 14:31:03 -0400

    creates skeleton for books-app

---
+++ seed.sql
@@ -0,0 +1,10 @@
+insert into books
+        (title, author, genre)
+values
+    ('Baby you''r gonna be mine', 'kevin Wilson', 'PBS must read'),
+    ('The Overstory', 'Richard Powers', 'PBS must read'),
+    ('The Line Becomes a River', 'Francisco Cantu', 'Autobiographical'),
+    ('Good and Mad', 'Rebecca Traister', 'lit'),
+    ('Crice', 'Madiline Miller', 'humor'),
+    ('Early Work', 'Andrew Martin', 'history'),
+;
